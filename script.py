e3c0486775446a91b214a397582c7bb5f6236f3c
Woody Rousseau <woody.a.rousseau@gmail.com>
2017-07-06 00:08:51 +0200

    Handle bpm detection

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import youtube_dl
-from flask import Flask
+import os
+from flask import Flask, request, url_for, send_from_directory
+from flask_cors import CORS
+from bpm_detector import get_bpm_from_file
 
 app = Flask(__name__)
+CORS(app)
 
 class MyLogger(object):
     def debug(self, msg):
@@ -17,24 +21,33 @@ class MyLogger(object):
 def my_hook(d):
     if d['status'] == 'finished':
         print('Done downloading, now converting ...')
-    print(d)
 
 
 ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
+        'preferredcodec': 'wav',
+        'preferredquality': '96',
     }],
-    'logger': MyLogger(),
-    'outtmpl': '%(id)s.%(ext)s',
+    'outtmpl': './files/%(id)s.%(ext)s',
     'progress_hooks': [my_hook],
 }
 
-@app.route("/")
-def hello():
+@app.route("/download/<string:youtube_id>")
+def download(youtube_id):
+    if os.path.isfile('./files/%s.wav' % youtube_id):
+        return url_for("send_files", path="%s.wav" % youtube_id, _external=True)
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
-        print(result)
-    return "Hello World!"
+        full_url = 'https://www.youtube.com/watch?v=%s' % youtube_id
+        result = ydl.download([full_url])
+        return url_for("send_files", path="%s.wav" % youtube_id, _external=True)
+
+@app.route('/files/<path:path>')
+def send_files(path):
+    return send_from_directory('files', path)
+
+@app.route('/files/<path:path>/bpm')
+def get_bpm(path):
+    bpm = get_bpm_from_file('./files/%s' % path)
+    return str(bpm)
